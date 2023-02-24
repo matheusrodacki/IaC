296e5634ebffcb790a6e1277a884d7eba5591c4f
matheusrodacki <matheus.rodacki@outlook.com>
2023-02-23 22:06:39 -0300

    Edit user_data

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   key_name = "IaC-Alura"
-  user_data =  <<-EOF
+  user_data = <<-EOF
                   #!/bin/bash
                   cd /home/ubuntu
-                  echo "<h1>Ola Mundo - Terraform</h1>" > index.html
+                  echo "<h1>Ola Mundo - Terraform - Que D+</h1>" > index.html
                   nohup busybox httpd -f -p 8080 &
                   EOF
   tags = {
-    Name = "Teste AWS"
+    Name = "Teste AWS2"
   }
 }
