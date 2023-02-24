a944de49991b6a328692b6c8a7255712c76f315d
matheusrodacki <matheus.rodacki@outlook.com>
2023-02-23 22:32:02 -0300

    Add Ansible files

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   key_name = "IaC-Alura"
-  user_data = <<-EOF
+  /* user_data = <<-EOF
                   #!/bin/bash
                   cd /home/ubuntu
                   echo "<h1>Ola Mundo - Terraform - Que D+</h1>" > index.html
                   nohup busybox httpd -f -p 8080 &
-                  EOF
+                  EOF */
   tags = {
     Name = "Teste AWS"
   }
