4ce69732c5e7114def4c7a02ef364e3e455fb63e
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-02-25 11:24:34 -0300

    copy ansible.sh

--- env/prod/ansible.sh
+++ env/prod/ansible.sh
@@ -5,48 +5,35 @@ sudo python3 get-pip.py
 sudo python3 -m pip install ansible
 tee -a playbook.yml > /dev/null <<EOT
 - hosts: localhost
-  tasks: 
-
-  - name: Install Python 3 and Virtualenv
+  tasks:
+  - name: Instalando o python3, virtualenv
     apt:
       pkg:
       - python3
       - virtualenv
       update_cache: yes
     become: yes
-
   - name: Git Clone
     ansible.builtin.git:
-      repo: https://github.com/alura-cursos/clientes-leo-api.git
+      repo: https://github.com/guilhermeonrails/clientes-leo-api.git
       dest: /home/ubuntu/tcc
       version: master
       force: yes
-      
-  - name: Install Django and Django Rest
+  - name: Instalando dependencias com pip
     pip:
       virtualenv: /home/ubuntu/tcc/venv
       requirements: /home/ubuntu/tcc/requirements.txt
-
-  - name: Alter hosts on settings
-    lineinfile: 
+  - name: Alterando o hosts do settings
+    lineinfile:
       path: /home/ubuntu/tcc/setup/settings.py
       regexp: 'ALLOWED_HOSTS'
-      line: "ALLOWED_HOSTS = ['*']"
+      line: 'ALLOWED_HOSTS = ["*"]'
       backrefs: yes
-
-  - name: DB Config
-    shell: 'cd ~/tcc; 
-            . venv/bin/activate;  
-            python /home/ubuntu/tcc/manage.py migrate'
-
-  - name: Load data
-    shell: 'cd ~/tcc; 
-            . venv/bin/activate;  
-            python /home/ubuntu/tcc/manage.py loaddata clientes.json'          
-
-  - name: Run Server
-    shell: 'cd ~/tcc; 
-            . venv/bin/activate; 
-            nohup python manage.py runserver 0.0.0.0:8000 &'
+  - name: configurando o banco de dados
+    shell: '. /home/ubuntu/tcc/venv/bin/activate; python /home/ubuntu/tcc/manage.py migrate'
+  - name: carregando os dados iniciais
+    shell: '. /home/ubuntu/tcc/venv/bin/activate; python /home/ubuntu/tcc/manage.py loaddata clientes.json'
+  - name: iniciando o servidor
+    shell: '. /home/ubuntu/tcc/venv/bin/activate; nohup python /home/ubuntu/tcc/manage.py runserver 0.0.0.0:8000 &'
 EOT
 ansible-playbook playbook.yml
